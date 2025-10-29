663de8135ec0c5fcfb8d89eb8f19b7b641d12ff7
GuilhermeXavier740 <cl204260@g.unicamp.br>
2025-10-29 19:20:27 -0300

    Atualização ItemVenda.cs

--- SysFin_2CTDS.Model/ItemVenda.cs
+++ SysFin_2CTDS.Model/ItemVenda.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace SysFin_2CTDS.Model
+namespace SysFin_2CTDS.Models
 {
-    internal class ItemVenda
+    public class ItemVenda
     {
+        public int Id { get; set; }
+        public int IdVenda { get; set; }
+        public int IdProduto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
     }
 }
